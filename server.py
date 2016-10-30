a25e997ab942de24b552dd6a3ef6a30ce3e32ac1
Ahmed masoud <ahmedmasoud@AhmedMasoud.local>
2016-10-30 23:43:35 +0200

    threads proto-type

--- server.py
+++ server.py
@@ -1,25 +1,12 @@
 import socket
+import thread
 
-# create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = "127.0.0.1"
-
-port = 8888
-
-# bind to the port
-serversocket.bind((host, port))
-
-# queue up to 5 requests
-serversocket.listen(5)
-
-while True:
-    # establish a connection
-    clientsocket,addr = serversocket.accept()
+def handleRequest(clientsocket,addr):
     print("Got a connection from %s" % str(addr))
     clientMsg = clientsocket.recv(512)
     print(clientMsg+"\n")
     webClient = False
+    # to know a client or browser and parse the request
     if '/' in clientMsg:
         webClient = True
         webRequest = clientMsg.split('\n')
@@ -36,7 +23,7 @@ while True:
         except:
             clientsocket.send("404 Not Found")
             clientsocket.close()
-            continue
+            return
         if not webClient:
             clientsocket.send("200 OK")
         while True:
@@ -58,3 +45,22 @@ while True:
     else:
         clientsocket.send("unknown command")
     clientsocket.close()
+
+# create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "127.0.0.1"
+
+port = 8888
+
+# bind to the port
+serversocket.bind((host, port))
+
+# queue up to 5 requests
+serversocket.listen(5)
+
+while True:
+    # establish a connection
+    clientsocket, addr = serversocket.accept()
+    thread.start_new_thread(handleRequest, (clientsocket, addr,))
+
